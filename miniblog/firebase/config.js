642f1bc0fa3862cd307c71542bc554a345ccba80
Débora Dias <debora.dias@terceiro.rnp.br>
2025-10-31 17:50:36 -0300

    adicionando configuracao firebase

---
+++ miniblog/firebase/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyATeXQARHJXsxwJZJbwgBOFiLXDwO9Xqcs",
+  authDomain: "miniblog-4c1c0.firebaseapp.com",
+  projectId: "miniblog-4c1c0",
+  storageBucket: "miniblog-4c1c0.firebasestorage.app",
+  messagingSenderId: "776154585415",
+  appId: "1:776154585415:web:2452ee18d85e1af8f05462"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
