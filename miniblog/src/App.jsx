067b0b95a519190e2866335aa9d356f0b6da9e98
Débora Dias <debora.dias@terceiro.rnp.br>
2025-10-31 18:22:43 -0300

    configurando projeto em vite

---
+++ miniblog/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+// pages
+import Home from './pages/Home/Home.jsx';
+import About from './pages/About/About.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
