1b33b86bed04d5cb4827b6d02890e1ca99e07265
Débora Dias <debora.dias@terceiro.rnp.br>
2025-11-03 12:28:01 -0300

    ajustes em inicializacao e adicao de pag de login e registro

--- miniblog/src/App.jsx
+++ miniblog/src/App.jsx
@@ -6,6 +6,8 @@ import Home from './pages/Home/Home.jsx';
 import About from './pages/About/About.jsx';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
+import Login from './pages/Login/Login.jsx';
+import Register from './pages/Register/Register.jsx';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </div>
         <Footer />
