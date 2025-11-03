cba93a24a35f702d30add1ad16866d0b3856cf47
Débora Dias <debora.dias@terceiro.rnp.br>
2025-11-03 13:31:25 -0300

    cria hook de autenticacao

---
+++ miniblog/src/hooks/useAuthentication.jsx
@@ -0,0 +1,24 @@
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut
+} from "firebase/auth";
+
+import { useState, useEffect } from "react";
+
+export const useAuthentication = () => {
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(null);
+
+    // cleanup
+    const [cancelled, setCancelled] = useState(false);
+
+    const auth = getAuth();
+
+    function checkIfCancelled() {
+        if (cancelled){
+            return;
+        }
+    }
+};
