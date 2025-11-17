ca51d6f1fdf4ddea00ed6b56c1f84c67860872bf
Débora Dias <debora.dias@terceiro.rnp.br>
2025-11-16 22:13:47 -0300

    exibe posts na pagina home

---
+++ miniblog/src/hooks/useFetchDocuments.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import {
+    collection,
+    getDocs, 
+    query, 
+    where, 
+    orderBy, 
+    snapshotEqual,
+    onSnapshot
+} from "firebase/firestore";
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+
+    const [documents, setDocuments] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    // deak with memory leak
+    const [cancelled, setCancelled] = useState(false);
+
+    useEffect(() => {
+
+        async function loadData() {
+            if (cancelled) return;
+
+            setLoading(true);
+
+            const collectionRef = await collection(db, docCollection);
+
+            try {
+                let q;
+
+                q = await query(collectionRef, orderBy("createdAt", "desc"));
+
+                await onSnapshot(q, (querySnapshot) => {
+                    setDocuments(
+                        querySnapshot.docs.map(doc => ({
+                            id: doc.id,
+                            ...doc.data(),
+                        }))
+                    );
+                });
+
+                setLoading(false);
+            } catch (error) {
+                console.log(error);
+                setError(error.message);
+
+                setLoading(false);
+            }
+        }    
+        loadData();
+    }, [docCollection, search, uid]);
+
+    useEffect(() => {
+        return () => setCancelled(true);
+    }, []);
+    return { documents, loading, error };
+}
--- miniblog/src/pages/Home/Home.jsx
+++ miniblog/src/pages/Home/Home.jsx
@@ -1,10 +1,43 @@
 // CSS
 import styles from './Home.module.css';
 
+// Hooks
+import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+import PostDetail from '../../components/PostDetail';
+
+// Components
+
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const {documents: posts, loading} = useFetchDocuments("posts");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Veja os posts mais recentes</h1>
+      <form className={styles.searchForm} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..." 
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div className={styles.posts}>
+        {(posts || []).map((post) => <PostDetail key={post.id} post={post} /> 
+        )}
+        {posts && posts.length === 0 && (
+          <div className={styles.noPosts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/post/create" className="btn">Criar primeiro post</Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
